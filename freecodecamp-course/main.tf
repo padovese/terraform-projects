1dff1e75909dff262c0baa691c3ba45fe2397717
padovese <bpadovese@uolinc.com>
2020-08-02 21:10:07 -0300

    reference resources

--- freecodecamp-course/main.tf
+++ freecodecamp-course/main.tf
@@ -1,4 +1,6 @@
-  provider "aws" {
+#https://www.youtube.com/watch?v=SLB_c_ayRMo
+
+provider "aws" {
   region  = "us-east-1"
   access_key = "-"
   secret_key = "-"
@@ -12,6 +14,8 @@
 
 # terraform plan = run your code for sintax and rules validation
 # terraform apply = run your code for real
+# terraform destroy = delete the resources 
+# (if you delete the code, TF will destroy the resource in the next apply command)
 
 resource "aws_instance" "ec2_by_terraform" {
     ami = "ami-0ac80df6eff0e70b5"
@@ -19,5 +23,24 @@ resource "aws_instance" "ec2_by_terraform" {
 
     tags = {
         Name = "padova"
+        Desc = "LOL"
     }
+}
+
+resource "aws_vpc" "first_vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "Production"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.first_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Production_subnet"
+  }
 }
